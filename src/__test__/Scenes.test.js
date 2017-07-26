a3a6f8d9396de3a81b725fda9785cef2c4546207
Miguel Alejandro <admin@miguelvinan.com>
2017-07-26 10:21:25 -0500

    FEAT: add jes for test components

--- src/__test__/Scenes.test.js
+++ src/__test__/Scenes.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Scenes from './Scenes';
+import Scenes from '../Scenes';
 
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const rendered = renderer.create(<Scenes />).toJSON();
-  expect(rendered).toBeTruthy();
+  expect( rendered ).toMatchSnapshot();
+  expect( rendered ).toBeTruthy();
 });
