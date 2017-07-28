e6d98035fb1d66711a31e7402e90cc96a718dff3
Miguel Alejandro <admin@miguelvinan.com>
2017-07-28 08:30:29 -0500

    FIX: add test to component

--- src/containers/Home/__test__/Home.test.js
+++ src/containers/Home/__test__/Home.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import Scenes from '../Scenes';
+import Home from '../Home';
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<Scenes />).toJSON();
+  const rendered = renderer.create(<Home />).toJSON();
   expect(rendered).toMatchSnapshot();
   expect(rendered).toBeTruthy();
 });
