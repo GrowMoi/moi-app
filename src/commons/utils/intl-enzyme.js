9cda8341b441f2e4a588e38a51cc03c625c74f63
Miguel Alejandro <admin@miguelvinan.com>
2017-07-28 15:17:51 -0500

    REF: Add support to react-intl for testing components

--- src/commons/utils/intl-enzyme.js
+++ src/commons/utils/intl-enzyme.js
@@ -8,39 +8,34 @@
 import React from 'react';
 import { IntlProvider, intlShape } from 'react-intl';
 import { mount, shallow } from 'enzyme';
+import { Text } from 'react-native';
 import flattenMessages from './flattenMessages';
 
-// You can pass your messages to the IntlProvider. Optional: remove if unneeded.
 import messages from '../../messages';
 
 // Create the IntlProvider to retrieve context for wrapping around.
 const intlProvider = new IntlProvider({
-  locale: 'en', messages: flattenMessages(messages.en),
+    locale: 'en-US', messages: flattenMessages(messages['en-US'])
 }, {});
+
 const { intl } = intlProvider.getChildContext();
 
 /**
  * When using React-Intl `injectIntl` on components, props.intl is required.
  */
 function nodeWithIntlProp(node) {
-  return React.cloneElement(node, { intl });
+    return React.cloneElement(node, { intl });
 }
 
-export function shallowWithIntl(node, { context } = {}) {
-  return shallow(
-    nodeWithIntlProp(node),
-    {
-      context: Object.assign({}, context, { intl }),
-    },
-  );
+export function shallowWithIntl(node) {
+    return shallow(nodeWithIntlProp(node), {
+        context: { intl }
+    });
 }
 
-export function mountWithIntl(node, { context, childContextTypes } = {}) {
-  return mount(
-    nodeWithIntlProp(node),
-    {
-      context: Object.assign({}, context, { intl }),
-      childContextTypes: Object.assign({}, { intl: intlShape }, childContextTypes),
-    },
-  );
+export function mountWithIntl(node) {
+    return mount(nodeWithIntlProp(node), {
+        context: { intl },
+        childContextTypes: { intl: intlShape }
+    });
 }
