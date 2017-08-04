42797874cca303486aa33225397f83abbc646b0d
Miguel Alejandro <admin@miguelvinan.com>
2017-08-04 15:37:45 -0500

    FEAT: Badge component

--- storybook/index.js
+++ storybook/index.js
@@ -7,6 +7,7 @@ const { hostname } = url.parse(NativeModules.SourceCode.scriptURL);
 // import stories
 configure(() => {
   require('./stories/stories/Moicon.story');
+  require('./stories/stories/Badge.story');
 }, module);
 
 const StorybookUI = getStorybookUI({ port: 7007, host: hostname, onDeviceUI: true });
