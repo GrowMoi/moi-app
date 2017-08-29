4173a1ac9746b3b8db2ed34018116930e271d820
Miguel <mvinan@icloud.com>
2017-08-29 07:47:21 -0500

    FEAT: implement action sheet on touch more button in tree view

--- src/containers/Tree/Tree.js
+++ src/containers/Tree/Tree.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
 import { Text, TouchableOpacity, View } from 'react-native';
-import { ScreenOrientation, Icon } from 'expo';
+import { ScreenOrientation } from 'expo';
 import { Actions } from 'react-native-router-flux';
 import Navbar from '../../commons/components/Navbar/Navbar';
 import MoiBackground from '../../commons/components/Background/MoiBackground';
-import { BottomBar, Maceta } from '../../commons/components/SceneComponents';
+import TreeBottom from './components/TreeBottom';
 
 const Content = styled(View)`
   flex: 1;
@@ -15,40 +15,9 @@ const Content = styled(View)`
   position: relative;
 `;
 
-const BottomContainer = styled(View)`
-  position: relative;
-`;
-
-const BarContainer = styled(View)`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-`;
-
-const MacetaContainer = styled(View)`
-  position: absolute;
-  align-items: center;
-  height: 100;
-  left: 0;
-  right: 0;
-  bottom: 0;
-`;
-
-const ButtonMore = styled(Icon.Ionicons)`
-  position: absolute;
-  bottom: 15;
-  right: 15;
-  background-color: transparent;
-`;
-
 export default class Tree extends Component {
   componentDidMount() {
     ScreenOrientation.allow('ALL');
-  }
-
-  showOptions = () => {
-    console.log('Press Options');
   }
 
   render() {
@@ -65,13 +34,7 @@ export default class Tree extends Component {
             </Text>
           </TouchableOpacity>
         </Content>
-        <BottomContainer>
-          <BarContainer><BottomBar/></BarContainer>
-          <MacetaContainer><Maceta width={200}/></MacetaContainer>
-          <TouchableOpacity onPress={this.showOptions}>
-            <ButtonMore name="ios-more" size={45} />
-          </TouchableOpacity>
-        </BottomContainer>
+        <TreeBottom />
       </MoiBackground>
     );
   }
