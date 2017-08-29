d57b805040cae239019120fe092f1d3fc5a6fc91
Miguel <mvinan@icloud.com>
2017-08-29 10:33:36 -0500

    FEAT: Add tree like static image

--- src/containers/Tree/Tree.js
+++ src/containers/Tree/Tree.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { View, Image } from 'react-native';
 import { ScreenOrientation } from 'expo';
-import { Actions } from 'react-native-router-flux';
 import Navbar from '../../commons/components/Navbar/Navbar';
 import MoiBackground from '../../commons/components/Background/MoiBackground';
 import TreeBottom from './components/TreeBottom';
+import treeStaticImage from '../../../assets/images/tree/static_tree.png';
 
 const Content = styled(View)`
   flex: 1;
-  justify-content: center;
+  justify-content: flex-end;
   align-items: center;
   position: relative;
+  z-index: 1;
+`;
+
+const treeImageAspectRatio = (4 / 5);
+const TreeImage = styled(Image)`
+  width: ${props => props.width || 250};
+  height: ${props => treeImageAspectRatio * props.width};
+  bottom: 50;
 `;
 
 export default class Tree extends Component {
@@ -25,14 +33,10 @@ export default class Tree extends Component {
       <MoiBackground>
         <Navbar/>
         <Content>
-          <TouchableOpacity>
-            <Text
-              color='white'
-              onPress={() => Actions.refresh({ key: 'moiDrawer', open: value => !value })}
-            >
-              Contenido
-            </Text>
-          </TouchableOpacity>
+          <TreeImage
+            width={300}
+            source={treeStaticImage}
+            resizeMode='contain' />
         </Content>
         <TreeBottom />
       </MoiBackground>
