f64aa284cb64ea9b0aff33642552e626d1e6259e
Miguel Viñan Hidalgo <miguelvinanhidalgo@Miguel.ioet-loja.com>
2017-09-06 06:19:24 -0500

    Add tree assets

--- assets/images/index.js
+++ assets/images/index.js
@@ -9,4 +9,17 @@ export default [
   require('./navbar/barra_superior_portrait.png'),
   require('./tabBar/tabBar.png'),
   require('./tree/static_tree.png'),
+
+  /**
+   * Moi Tree
+   */
+
+  // Level 1
+  require('./tree/nivel_1/nivel_1_descubierta.png'),
+
+  // Level 2
+  require('./tree/nivel_2/nivel_2_descubierta_r1.png'),
+  require('./tree/nivel_2/nivel_2_descubierta_r2.png'),
+  require('./tree/nivel_2/nivel_2_descubierta_r3.png'),
+  require('./tree/nivel_2/nivel_2_descubierta_r4.png'),
 ];
