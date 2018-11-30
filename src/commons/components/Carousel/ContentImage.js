a45dea6408535b941892e66244a37df8b9542286
Miguel Viñan Hidalgo <mvinan@icloud.com>
2018-11-30 12:12:55 -0500

    FIX: add play icon to thumbnail video

--- src/commons/components/Carousel/ContentImage.js
+++ src/commons/components/Carousel/ContentImage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { View, TouchableWithoutFeedback, ImageBackground } from 'react-native';
 import styled from 'styled-components/native';
+import { FontAwesome } from '@expo/vector-icons'
 import Preloader from '../Preloader/Preloader';
 
 const Container = styled(TouchableWithoutFeedback)`
@@ -12,6 +13,8 @@ const Container = styled(TouchableWithoutFeedback)`
 
 const Img = styled(ImageBackground)`
   flex: 1;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default class ContentImage extends Component {
@@ -25,12 +28,18 @@ export default class ContentImage extends Component {
   }
 
   render() {
-    const { size, onPressImage, ...rest } = this.props;
+    const { size, onPressImage, data, ...rest } = this.props;
 
     return (
-      <Container size={size} onPress={() => !this.state.loading && this.onPress({ ...rest, size })}>
+      <Container size={size} onPress={() => !this.state.loading && this.onPress({ ...rest, data, size })}>
         <Img {...rest} onLoad={() => this.setState({ loading: false })} onError={() => this.setState({ loading: false })}>
           {this.state.loading && <Preloader notFullScreen/>}
+          {data.type === 'video' && (
+            <FontAwesome
+              name="play-circle"
+              size={40}
+              color={'rgba(255, 255, 255, 0.4)'}
+            />)}
         </Img>
       </Container>
     );
