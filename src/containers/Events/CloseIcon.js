911511c022f82ddde340f0c2ea27757f5a81cb66
Miguel Viñan Hidalgo <mvinan@icloud.com>
2019-05-30 23:36:14 -0500

    Change event modal icon

--- src/containers/Events/CloseIcon.js
+++ src/containers/Events/CloseIcon.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, ImageBackground } from 'react-native';
 import styled from 'styled-components/native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
@@ -7,21 +7,25 @@ const Container = styled(TouchableOpacity)`
   position: absolute;
   top: 0;
   right: 0;
-  width: 25;
-  height: 25;
+  width: 35;
+  height: 35;
   zIndex: 1;
-  background-color: #D69F2B;
-  border-radius: 35;
-  border: 1px #E3BB83;
 `;
 
-const Icon = styled(Ionicons)`
-  position: absolute;
-  top: 2;
-  right: 6;
+
+const Icon = styled(ImageBackground)`
+  width: 35;
+  height: 35;
   zIndex: 1;
-  font-weight: 900;
 `;
+
+// const Icon = styled(Ionicons)`
+//   position: absolute;
+//   top: 2;
+//   right: 6;
+//   zIndex: 1;
+//   font-weight: 900;
+// `;
 
 export default class CloseIcon extends Component {
   render() {
@@ -32,10 +36,15 @@ export default class CloseIcon extends Component {
         onPress={onPress}
         activeOpacity={0.8}
       >
-        <Icon name='md-close'
+        <Icon
+          onPress={this.removeResultFinalTest}
+          source={{ uri: 'boton_salir_h' }}
+          // style={style}
+          resizeMode='stretch' />
+        {/* <Icon name='md-close'
           color='white'
           size={18}
-        />
+        /> */}
       </Container>
     );
   }
