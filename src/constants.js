daf358e0272a0895be4d697c80da0f7957b2060b
Miguel Viñan Hidalgo <mvinan@icloud.com>
2019-08-28 13:03:12 -0500

    FIX: update url constants

--- src/constants.js
+++ src/constants.js
@@ -4,6 +4,6 @@ export const FLORECIDA = 'florecida';
 export const CONTENT_TYPE_RECOMMENDED = 'recommended'
 export const TIME_FOR_INACTIVITY = 12000;
 // export const URL_BASE = __DEV__ ? 'http://moi-backend.growmoi.com' : 'http://moi-backend.growmoi.com';
-export const URL_BASE = __DEV__ ? 'http://moi-backend.growmoi.com' : 'http://moi-backend.growmoi.com';
+export const URL_BASE = __DEV__ ? 'https://moi-staging.herokuapp.com' : 'http://moi-backend.growmoi.com';
 export const WEB_URL_BASE = __DEV__ ? 'http://moi-frontend.herokuapp.com' : 'http://moi.growmoi.com';
 export const CLOUDINARY_BASE = 'https://api.cloudinary.com';
