54816d45bb7106efbdc347e3e569146bec1c50f3
Jhymer Martínez <jmartinez@ioet.com>
2019-11-26 08:18:27 -0500

    Add moi-pacifico videos

---
+++ assets/videos/index.js
@@ -0,0 +1,14 @@
+export default {
+  '-UjQ2-LRqlA': require('./-UjQ2-LRqlA.mp4'),
+  '0y5quQ7YGsw': require('./0y5quQ7YGsw.mp4'),
+  '2IHatT_RPIE': require('./2IHatT_RPIE.mp4'),
+  '7blLLrleK20': require('./7blLLrleK20.mp4'),
+  '8_SNkemgya4': require('./8_SNkemgya4.mp4'),
+  '77sLP_B4EBI': require('./77sLP_B4EBI.mp4'),
+  'AXk5kd9DGZQ': require('./AXk5kd9DGZQ.mp4'),
+  'fHR62T7WeCM': require('./fHR62T7WeCM.mp4'),
+  'm1w1LKMS9SI': require('./m1w1LKMS9SI.mp4'),
+  'nk0HPH5s_tc': require('./nk0HPH5s_tc.mp4'),
+  'PKzNSqKD5-4': require('./PKzNSqKD5-4.mp4'),
+  'xafSVJOPoDw': require('./xafSVJOPoDw.mp4'),
+}
