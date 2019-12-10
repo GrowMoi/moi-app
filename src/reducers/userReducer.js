1ad79ea9a9a81762ae9e8d3fc5fd81e949c2bae3
Miguel Viñan Hidalgo <mvinan@icloud.com>
2019-12-10 11:02:35 -0500

    UPDATE: bundle identifier and remove mocked data

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import { object } from '../commons/utils';
 import * as actionTypes from '../actions/actionTypes';
-import qr from '../mocks/quiz-results'
 
 export const initialState = {
   userData: {
@@ -33,7 +32,7 @@ export const initialState = {
   },
   events: [],
   eventsWeek: [],
-  quizResult: qr,
+  quizResult: null,
   contentsToLearn: {
     contents: {},
     meta: {},
