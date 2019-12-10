f012f844be5c64a7cf6cde49a0545c7019f21a6c
Miguel Viñan Hidalgo <mvinan@icloud.com>
2019-12-10 10:14:59 -0500

    UPDATE: wip update bundle identifiers ios, android

--- android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/AppConstants.java
@@ -37,6 +37,7 @@ public class AppConstants {
         
         
         
+        
         // ADD EMBEDDED RESPONSES HERE
         // START EMBEDDED RESPONSES
         embeddedResponses.add(new Constants.EmbeddedResponse("https://exp.host/@moi/moi-pacifico", "assets://shell-app-manifest.json", "application/json"));
