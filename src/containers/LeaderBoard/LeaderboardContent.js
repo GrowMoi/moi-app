75bd62d53382a9f644895f15b70089cb8f9f90e2
Jhymer Martínez <jhmartinez1991@gmail.com>
2020-02-29 20:09:34 -0500

    Add dropdown to select keywords

--- src/containers/LeaderBoard/LeaderboardContent.js
+++ src/containers/LeaderBoard/LeaderboardContent.js
@@ -1,23 +1,38 @@
 import React, { PureComponent } from 'react';
 import styled from 'styled-components/native';
 import { connect } from 'react-redux';
-import uuid from 'uuid/v4';
-import { View, FlatList, StyleSheet, Alert } from 'react-native';
+import { View, FlatList, StyleSheet, Alert, Text, TouchableHighlight } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-import { Size, Palette } from '../../commons/styles';
+import { Palette } from '../../commons/styles';
 import LeaderRow from '../../commons/components/LeaderRow/LeaderRow';
 import { normalize, object } from '../../commons/utils';
 import Preloader from '../../commons/components/Preloader/Preloader';
 import { FontAwesome } from '@expo/vector-icons';
-
+import _ from 'lodash';
 // Actions
 import profilesActions from '../../actions/profileActions';
 import treeActions from '../../actions/treeActions';
-import { ContentBox } from '../../commons/components/ContentComponents';
 import leaderboardActions from '../../actions/leaderboardActions';
 import deviceUtils from '../../commons/utils/device-utils';
 
 const isTablet = deviceUtils.isTablet();
+
+const FilterContainer = styled(View)`
+  height: 30;
+`;
+
+const Dropdown = styled(View)`
+  border: 1px solid ${Palette.white};
+  border-radius: 5px;
+  top: -10px;
+  padding: 0 15px 0 10px;
+  background-color: ${Palette.colors.blue};
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 120px;
+`;
 
 const ContentContainer = styled(View)`
   width: ${isTablet ? '85%' : '96%'};
@@ -37,18 +52,46 @@ class LeaderBoardContent extends PureComponent {
   state = {
     isLoadingProfile: false,
     loading: true,
+    showModal: false,
+    sortItem: '',
   }
 
   isSuperEventLeaderBoard;
 
+  _reload = async (newParams, item) => {
+    this.setState({
+      loading: true,
+      sortItem: item,
+    });
+    const { getLeaderboardAsync } = this.props;
+    try {
+      await getLeaderboardAsync(newParams, 1);
+      this.setState({loading: false});
+    } catch(err) {
+      this.setState({loading: false});
+    }
+  }
+
   async componentDidMount() {
-    const { getLeaderboardAsync, leaderboardParams } = this.props;
+
+    const { getLeaderboardAsync, leaderboardParams, actions } = this.props;
+    if (actions && actions.onLoadComponent) {
+      actions.onLoadComponent(this._reload);
+    }
+
     await getLeaderboardAsync(leaderboardParams, 1);
     this.setState({loading: false});
   }
 
   fetchNextPage = async () => {
     const { loadMoreLeadersAsync, leaderboardParams } = this.props;
+    const { sortItem } = this.state;
+
+    if (!_.isEmpty(sortItem)) {
+      const { sort_by: sortBy } = leaderboardParams;
+      leaderboardParams[sortBy] = sortItem;
+    }
+
     await loadMoreLeadersAsync(leaderboardParams);
   }
 
@@ -117,15 +160,22 @@ class LeaderBoardContent extends PureComponent {
       />
     )
   }
+
   render() {
     const {
       leaders: {
         leaders: allLeaders,
-        meta: { user_data: userLeader }
+        meta: {
+          user_data: userLeader,
+          sort_by_options: sortOptions
+        }
       },
       closeModal,
+      leaderboardParams,
+      actions,
     } = this.props;
-    const { isLoadingProfile, loading } = this.state;
+
+    const { isLoadingProfile, loading, sortItem } = this.state;
 
     const styles = StyleSheet.create({
       contentContainer: {
@@ -138,30 +188,44 @@ class LeaderBoardContent extends PureComponent {
 
     if(loading) return <Preloader notFullScreen/>;
 
+    const { label, sort_by: sortBy} = leaderboardParams;
+    const { showDropdownModal } = actions;
+    const { values: sortValues } = sortOptions || {};
+
     return (
-      <ContentContainer>
-        {(allLeaders || []).length > 0 && (
-          <FlatList
-            style={{ height: '90%', top: -20 }}
-            contentContainerStyle={styles.contentContainer}
-            onEndReached={this.fetchNextPage}
-            onEndReachedThreshold={1}
-            data={allLeaders}
-            renderItem={this._renderItem}
-            keyExtractor={this._keyExtractor}
-          />
-        )}
-        {isLoadingProfile && <Preloader />}
-        <ContentFooter>
-          <FontAwesome name='ellipsis-h' size={15} color={Palette.white.css()}/>
-          <LeaderRow
-            position={userLeader.position}
-            playerName={normalize.normalizeAllCapLetter(userLeader.username)}
-            grade={userLeader.contents_learnt}
-            seconds={`${new Date(userLeader.time_elapsed).getSeconds()}s`}
-          />
-        </ContentFooter>
-      </ContentContainer>
+      <React.Fragment>
+        <FilterContainer>
+          <TouchableHighlight onPress = {() => { showDropdownModal(label, sortValues, sortBy) }}>
+            <Dropdown>
+              <Text style={{color: 'white'}}>{sortItem}</Text>
+              <FontAwesome name='chevron-down' size={10} color="white" style={{position: 'absolute', right: 0}}/>
+            </Dropdown>
+          </TouchableHighlight>
+        </FilterContainer>
+        <ContentContainer>
+          {(allLeaders || []).length > 0 && (
+            <FlatList
+              style={{ height: '90%', top: -10 }}
+              contentContainerStyle={styles.contentContainer}
+              onEndReached={this.fetchNextPage}
+              onEndReachedThreshold={1}
+              data={allLeaders}
+              renderItem={this._renderItem}
+              keyExtractor={this._keyExtractor}
+            />
+          )}
+          {isLoadingProfile && <Preloader />}
+          <ContentFooter>
+            <FontAwesome name='ellipsis-h' size={15} color={Palette.white.css()}/>
+            {userLeader && <LeaderRow
+              position={userLeader.position}
+              playerName={normalize.normalizeAllCapLetter(userLeader.username)}
+              grade={userLeader.contents_learnt}
+              seconds={`${new Date(userLeader.time_elapsed).getSeconds()}s`}
+            />}
+          </ContentFooter>
+        </ContentContainer>
+      </React.Fragment>
     );
   }
 }
