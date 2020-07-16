25e10ce6babddde1305dabcf3add0084a9551b87
Miguel Viñan Hidalgo <mvinan@icloud.com>
2020-07-16 13:49:21 -0500

    clean project

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++
@@ -1,8 +0,0 @@
-package host.exp.exponent.generated;
-
-// This file is auto-generated. Please don't rename!
-public class DetachBuildConstants {
-
-  public static final String DEVELOPMENT_URL = "expcb94a1b685d94c71b27eedc762636f73://192.168.1.17:19000";
-
-}
--- src/commons/components/SceneComponents/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Maceta } from './Maceta';
-export { default as BottomBar } from './BottomBar';
-export { default as BackButton } from './BackButton';
-export { default as BackButtonContainer } from './BackButtonContainer';
-export { default as BottomBarWithButtons } from './BottomBarWithButtons';
-export { default as WoodTitle } from './WoodTitle';
-export { default as Line } from './Line';
-export { default as InterestBox } from './InterestBox';
